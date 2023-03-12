401d81aec45ab3821be534a883e5304a8bfe76b8
Yuri <yuriyra@gmail.com>
2023-03-12 03:21:12 -0400

    Feature Complete

---
+++ FinanceApp_BackFront/FinanceApp_BackFront/Relatorios.swift
@@ -0,0 +1,29 @@
+//
+//  Relatorios.swift
+//  FinanceApp_BackFront
+//
+//  Created by Yuri Alencar on 2023-03-12.
+//
+
+import UIKit
+
+class Relatorios: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
