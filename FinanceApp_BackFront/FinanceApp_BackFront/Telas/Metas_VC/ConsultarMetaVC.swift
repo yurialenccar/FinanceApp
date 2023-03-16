2cc685659d7a9e97ea792d7435e2dd2fbedab307
Yuri <yuriyra@gmail.com>
2023-03-15 23:26:42 -0400

    nova tela - conferir a meta

---
+++ FinanceApp_BackFront/FinanceApp_BackFront/Telas/Metas_VC/ConsultarMetaVC.swift
@@ -0,0 +1,29 @@
+//
+//  ConsultarMetaVC.swift
+//  FinanceApp_BackFront
+//
+//  Created by Yuri Alencar on 2023-03-15.
+//
+
+import UIKit
+
+class ConsultarMetaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
