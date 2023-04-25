0664093de22465b1a754495c7ace61d3b26ec470
Gabriel Romano <gromanoluz@gmail.com>
2023-04-25 00:09:52 -0300

    Replicando a tela de Despesas para a de receitas receber todas suas funcionalidades.

--- FinanceApp_BackFront/FinanceApp_BackFront/Telas/AddAccountTransactionsScreens/RegisterIncomeScreen/RegisterIncomeScreen.swift
+++ FinanceApp_BackFront/FinanceApp_BackFront/Telas/AddAccountTransactionsScreens/RegisterIncomeScreen/RegisterIncomeScreen.swift
@@ -9,47 +9,124 @@ import UIKit
 
 class RegisterIncomeScreen: UIViewController {
     
+    var viewModel:AddAccountTransactionsViewModel=AddAccountTransactionsViewModel(type: .income)
+    
     private var indexCategorySelected:Int = 0
+    private var indexAccountSelected:Int = 0
 
     @IBOutlet weak var descTextField: UITextField!
     @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var dateField: UITextField!
+    @IBOutlet weak var categoryBackgroung: UIView!
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var accountLabel: UILabel!
+    @IBOutlet weak var bankLabel: UILabel!
+    @IBOutlet weak var accountBackground: UIView!
     @IBOutlet weak var obsTextField: UITextField!
-    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupDataPicker()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let today = Date()
+        dateField.text = viewModel.datePickerChange(date: today)
+        
+        updateCategoryField(indexCategorySelected)
+        updateAccountField(indexAccountSelected)
+        
+    }
+    @IBAction func tappedCategoryButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "CategoriesModalScreen", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "CategoriesModalScreen") {coder -> CategoriesModalScreen? in
+            return CategoriesModalScreen(coder: coder, transactionType: .income)
+        }
+        vc.delegate = self
+        if let presentationController = vc.presentationController as? UISheetPresentationController{
+            presentationController.detents = [.medium()]
+        }
+        self.present(vc, animated: true)
+    }
+    
+    @IBAction func tappedAccountButton(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "AccountsModalScreen", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "AccountsModalScreen") as? AccountsModalScreen
+        vc?.delegate = self
+        if let presentationController = vc?.presentationController as? UISheetPresentationController{
+            presentationController.detents = [.medium()]
+        }
+        self.present(vc ?? UIViewController(), animated: true)
+    }
+    
+    @IBAction func tappedRegisterButton(_ sender: UIButton) {
 
-        // Do any additional setup after loading the view.
+        if stringIsEmpty(text: amountTextField.text ?? ""){
+            amountTextField.layer.borderColor = UIColor.red.cgColor
+            amountTextField.layer.borderWidth = 1
+            showAlert(title: "Opa, esqueceu de informar valor gasto!")
+        } else {
+            viewModel.setTransactionsValues(
+                desc: descTextField.text ?? "",
+                amount: amountTextField.text!,
+                category: indexCategorySelected,
+                account: indexAccountSelected,
+                Obs: obsTextField.text ?? ""
+            )
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func updateCategoryField(_ indexCategory:Int){
+        categoryLabel.text = viewModel.getCategoryLabel(indexCategory)
+        categoryImage.image = viewModel.getCategoryImageName(indexCategory)
+        categoryBackgroung.backgroundColor = viewModel.getCategoryBackgroungColor(indexCategory)
+    }
+    
+    func updateAccountField(_ indexAccount:Int){
+        accountLabel.text = viewModel.getAccountLabel(indexAccount)
+        bankLabel.text = viewModel.getBankLabelText(indexAccount)
+        bankLabel.textColor = viewModel.getBankLabelColor(indexAccount)
+        accountBackground.backgroundColor = viewModel.getBankBackColor(indexAccount)
+    }
+    
+    private func setupDataPicker(){
+        let datePicker = UIDatePicker ()
+        datePicker.datePickerMode = .date
+        datePicker.addTarget(self, action: #selector(dateChange (datePicker:)), for: UIControl.Event.valueChanged)
+        datePicker.frame.size = CGSize(width: 0, height: 300)
+        datePicker.preferredDatePickerStyle = .inline
+        dateField.inputView = datePicker
+    }
+    
+    @objc func dateChange (datePicker: UIDatePicker) {
+        dateField.text = viewModel.datePickerChange(date: datePicker.date)
+        dateField.resignFirstResponder()
+    }
+    
+    private func showAlert (title: String) {
+        let alertController = UIAlertController (title: title, message: nil, preferredStyle: .alert)
+        let okButton = UIAlertAction (title: "Ok", style: .default, handler: nil)
+        alertController.addAction(okButton)
+        self.present(alertController, animated: true, completion: nil)
     }
     
 
-    @IBAction func tappedRegisterButton(_ sender: UIButton) {
+}
 
-        var missingInformations = false
-        var newTransaction = Transactions(desc: "Vazio", amount: 1.0, categoryIndex: 0, date: "01/01/2023", type: .income, accountIndex:0, obs: "")
-        
-        if (stringIsEmpty(text: descTextField.text ?? "")){
-            descTextField.layer.borderColor = UIColor.red.cgColor
-            descTextField.layer.borderWidth = 1
-            missingInformations = true
-            
-        } else{
-            newTransaction.desc=descTextField.text ?? ""
-        }
-        
-        if (stringIsEmpty(text: amountTextField.text ?? "")){
-            amountTextField.layer.borderColor = UIColor.red.cgColor
-            amountTextField.layer.borderWidth = 1
-            missingInformations = true
-        } else{
-            newTransaction.amount = Double(amountTextField.text ?? "0.0")!
-        }
-        
-        if missingInformations == false {
-            transactions.append(newTransaction)
-            dismiss(animated: true, completion: nil)
-        }
+//Extensão que busca a escolha do usuário das telas modais de seleção.
+extension RegisterIncomeScreen:CategoriesModalScreenDelegate, AccountsModalScreenDelegate {
+    func didSelectCategory(_ indexCategory: Int) {
+        indexCategorySelected = indexCategory
+        updateCategoryField(indexCategorySelected)
+    }
+    
+    func didSelectAccount(_ indexAcccount: Int) {
+        indexAccountSelected = indexAcccount
+        updateAccountField(indexAccountSelected)
     }
 }
