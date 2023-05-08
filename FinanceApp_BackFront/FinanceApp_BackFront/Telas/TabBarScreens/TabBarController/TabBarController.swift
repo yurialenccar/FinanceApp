689443fc4f0962b78446c93d45e7779a89fd5b48
Gabriel Romano <gromanoluz@gmail.com>
2023-05-08 16:53:03 -0300

    Criados Sub grupos para organização das telas dentro do App. Telas divididas entre: LoginScreens, TabbarScreens, ModalScreens, ErrorScreens

---
+++ FinanceApp_BackFront/FinanceApp_BackFront/Telas/TabBarScreens/TabBarController/TabBarController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  FinanceApp_BackFront
+//
+//  Created by Gabriel Luz Romano on 11/04/23.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    static let identifier:String = String(describing: TabBarController.self)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
