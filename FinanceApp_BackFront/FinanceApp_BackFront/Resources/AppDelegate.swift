c445c00f80f78a68b7a73cf1c971770cb43002aa
Gabriel Romano <gromanoluz@gmail.com>
2023-05-26 10:54:34 -0300

    Acrescentando Framework do Firebase para realizar autenticação de usuarios.

--- FinanceApp_BackFront/FinanceApp_BackFront/Resources/AppDelegate.swift
+++ FinanceApp_BackFront/FinanceApp_BackFront/Resources/AppDelegate.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import FirebaseCore
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
