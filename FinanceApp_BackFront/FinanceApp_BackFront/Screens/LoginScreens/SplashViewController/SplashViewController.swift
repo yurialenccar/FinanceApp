87907e6713fb130b07672c77ba3ac239435de57d
Gabriel Romano <gromanoluz@gmail.com>
2023-06-04 10:43:19 -0300

    Lottie adicionada à tela splash do Aplicativo.

--- FinanceApp_BackFront/FinanceApp_BackFront/Screens/LoginScreens/SplashViewController/SplashViewController.swift
+++ FinanceApp_BackFront/FinanceApp_BackFront/Screens/LoginScreens/SplashViewController/SplashViewController.swift
@@ -6,22 +6,49 @@
 //
 
 import UIKit
+import Lottie
 
 class SplashViewController: UIViewController {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    var animationView: LottieAnimationView = .init(name: "51720-making-money")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupLottie()
+        setupConstraints()
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2){
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3){
 
             let vc: MainViewController? = UIStoryboard(name: MainViewController.identifier, bundle: nil).instantiateViewController(withIdentifier: MainViewController.identifier) as? MainViewController
             self.navigationController?.pushViewController(vc ?? UIViewController(), animated: true)
-            
-            
+
+
         }
+    }
+    
+    private func setupLottie() {
+        //animationView = .init(name: "79808-green-money-falling")
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.frame = view.frame
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.animationSpeed = 1.0
+        view.addSubview(animationView)
+        animationView.play()
+        
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            animationView.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: -100),
+            animationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            animationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            animationView.heightAnchor.constraint(equalToConstant: 200),
+        ])
     }
     
 
