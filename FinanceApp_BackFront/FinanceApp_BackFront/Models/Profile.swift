af41d23ed8980a7661040f7b2703375c9a782251
Gabriel Romano <gromanoluz@gmail.com>
2023-07-06 02:47:02 -0300

    Model de profile do Firebase

---
+++ FinanceApp_BackFront/FinanceApp_BackFront/Models/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  FinanceApp_BackFront
+//
+//  Created by Gabriel Luz Romano on 06/07/23.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var email: String
+    var telefone: String
+}
